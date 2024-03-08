36c9f236e9312bf3d34b09dc7e7685e626b276ca
Kevin <identityofsine@gmail.com>
2024-03-08 11:09:35 -0500

    added git ignore

--- src/tagger.js
+++ src/tagger.js
@@ -1,7 +1,18 @@
 console.log('[tagger framework] loaded');
 
-function tag(tag, ...children) {
+function tag(tag, attr, ...children) {
 	const element = document.createElement(tag);
 	element.children = children;
+
+	if (attr) {
+		Object.keys(attr).forEach((key) => {
+			element.setAttribute(key, attr[key]);
+		});
+	}
+
 	return element;
+}
+
+function div(...children) {
+	return tag('div', ...children);
 }
